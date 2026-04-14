a4ae366966338b438650600deb9d735efd8c68f2
Dhruvil <dhruvilgandhi1203@gmail.com>
2026-04-14 23:27:32 +0530

    completed exercise about using props in card_project

--- Tutorials/src/App.jsx
+++ Tutorials/src/App.jsx
@@ -1,4 +1,4 @@
 // import Chapter1 from "./chapters/chapter-1/App";
-import Chapter2 from "./chapters/chapter-2/App";
-
-export default Chapter2;
+// import Chapter2 from "./chapters/chapter-2/App";
+import JobCard from "./card_project/App"
+export default JobCard;
