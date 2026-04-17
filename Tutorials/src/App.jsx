c4b4078ab5e85181c0c68f1497ac78cdb3c898d9
Dhruvil <dhruvilgandhi1203@gmail.com>
2026-04-18 00:26:37 +0530

    completed chapter-3(small intro to tailwind)

--- Tutorials/src/App.jsx
+++ Tutorials/src/App.jsx
@@ -1,4 +1,5 @@
 // import Chapter1 from "./chapters/chapter-1/App";
 // import Chapter2 from "./chapters/chapter-2/App";
-import JobCard from "./card_project/App"
-export default JobCard;
+// import JobCard from "./card_project/App"
+import Chapter3 from "./chapters/chapter-3/App";
+export default Chapter3;
---
+++ Tutorials/src/chapters/chapter-3/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-700px w-full bg-gray-400'>App</div>
+  )
+}
+
+export default App
